5b78c0a028fc5c1ac501b5d12b669c30412b40ca
manoelfranklin98 <129801230+manoelfranklin98@users.noreply.github.com>
2023-08-29 16:54:58 -0300

    Add files via upload

---
+++ br.edu.fiescfafic.poo.casa/src/br/edu/fiescfafic/poo/main/Main.java
@@ -0,0 +1,21 @@
+package br.edu.fiescfafic.poo.main;
+import br.edu.fiescfafic.poo.model.casa;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        casa nova = new casa("matadouro",2,"azul",2,2,5);
+
+
+        System.out.println("o endereco e:"+endereco);
+        System.out.println( " quantidadev de cozinhas"+quantidadeDeCozinhas);
+        System.out.println("a cor da casa e :"+cor);
+        System.out.println("quantidade de quartos"+quantidadeDeQuartos);
+        System.out.println("quantidade de banheiros"+quantidadeDeBanheiros);
+        System.out.println("quantidade de portas"quantidadeDePortas);
+
+    }
+}
+
+
